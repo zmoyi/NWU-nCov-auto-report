8227d32202fafb97948b59ad05290dc19aaca1da
zmoyi <48953751+zmoyi@users.noreply.github.com>
2022-01-12 00:26:31 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -237,11 +237,11 @@ def sent_report(cookies):
     params = {
         "sfzx":"1", #是否在校
         "tw":"1",   #体温（list）(0-"Below 36";1-"36-36.5";2-"36.5-36.9";3-"36.9-37.3"; ... , i<=8)
-        "area":"陕西省 西安市 长安区",
+        "area":"陕西省 西安市 莲湖区",
         "city":"西安市",
         "province":"陕西省",
-        "address":"陕西省西安市长安区郭杜街道西北大学南校区学生公寓10号楼西北大学长安校区",
-        "geo_api_info":'{"type":"complete","info":"SUCCESS","status":1,"$Da":"jsonp_687452_","position":{"Q":34.14218,"R":108.87518999999998,"lng":108.87519,"lat":34.14218},"message":"Get ipLocation success.Get address success.","location_type":"ip","accuracy":null,"isConverted":true,"addressComponent":{"citycode":"029","adcode":"610116","businessAreas":[],"neighborhoodType":"","neighborhood":"","building":"","buildingType":"","street":"文苑南路","streetNumber":"11号","country":"中国","province":"陕西省","city":"西安市","district":"长安区","township":"郭杜街道"},"formattedAddress":"陕西省西安市长安区郭杜街道西北大学南校区学生公寓10号楼西北大学长安校区","roads":[],"crosses":[],"pois":[]}',   #高德SDK返回值
+        "address":"陕西省西安市莲湖区西关街道怀德巷西北大学桃园校区",
+        "geo_api_info":'{"type":"complete","info":"SUCCESS","status":1,"$Da":"jsonp_687452_","message":"Get ipLocation success.Get address success.","location_type":"ip","accuracy":null,"isConverted":true,"addressComponent":{"citycode":"029","adcode":"610104","businessAreas":[],"neighborhoodType":"","neighborhood":"","building":"","buildingType":"","street":"高新四路","streetNumber":"145号","country":"中国","province":"陕西省","city":"西安市","district":"莲湖区","towncode":"610104006000","township":"西关街道"},"formattedAddress":"陕西省西安市莲湖区西关街道怀德巷西北大学桃园校区","roads":[],"crosses":[],"pois":[]}',   #高德SDK返回值
         "sfcyglq":"0",  #是否隔离期
         "sfyzz":"0",    #是否有症状
         "qtqk":"",  #其他情况
